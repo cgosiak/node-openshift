8689501beefac7f206d31f0cdb8beb25d7254f91
Caleb Gosiak <calebg@stearnsbank.com>
2018-08-22 15:23:16 -0500

    Added name

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var http = require('http');
 
 var port = process.env.PORT || 8080;
 
+const name = process.env.NAME || "OpenShift";
+
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello from OpenShift\n");
+  response.end(`Hello ${name}!\n`);
 }).listen(port);
 
 console.log("Server running on port " + port);
